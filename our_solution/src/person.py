2bece00bc94a1bef66054c34896ae1008351326a
Andrew High <andrewhigh4@hotmail.com>
2021-04-06 15:36:43 +0100

    working on advanced extensions

--- our_solution/src/person.py
+++ our_solution/src/person.py
@@ -1,5 +1,21 @@
 class Person:
-    def __init__(self, name, age):
+    def __init__(self, name, age, destination):
         self.name = name
         self.age = age
+        self.destination = destination
+
+    def check_destination_of_bus(self, bus):
+        if bus.destination == self.destination:
+            return True
+
+    def get_on_bus(self, bus_stop):
+        bus_stop.queue.append(self)
+        if self.check_destination_of_bus(bus) == True:
+            bus.pick_up_from_stop(bus_stop)
+            
+
+
+        
+
+        
     
--- our_solution/tests/person_test.py
+++ our_solution/tests/person_test.py
@@ -1,9 +1,10 @@
 import unittest
 from src.person import Person
+from src.bus import Bus
 
 class TestPerson(unittest.TestCase):
     def setUp(self):
-        self.person = Person("Guido van Rossum", 64)
+        self.person = Person("Guido van Rossum", 64, "Waverly Station")
 
     # @unittest.skip("Delete this line to run the test")
     def test_person_has_name(self):
@@ -12,3 +13,11 @@ class TestPerson(unittest.TestCase):
     # @unittest.skip("Delete this line to run the test")
     def test_person_has_age(self):
         self.assertEqual(64, self.person.age)
+
+    def test_destination_of_bus(self):
+        bus = Bus(22, "Waverly Station")
+        self.assertEqual (True, self.person.check_destination_of_bus(bus))
+
+    def test_get_on_bus(self):
+        bus = Bus(22, "Waverly Station")
+        self.assertEqual (1, len(bus.passengers))
